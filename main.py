8486af48a8f8a15e233f528fcb70d5f33e931f8c
STWtp <1582160745@qq.com>
2025-08-28 04:48:09 +0800

    new feature: 使用了先进的yaml文件格式取代py文件格式的jmcomic配置:)

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from ncatbot.core import BotClient
 from ncatbot.utils.config import config
 from ncatbot.utils.logger import get_log
 
+import ncatbot_config as ncatbot_config
+
 
 class NcatBot(BotClient):
     def __init__(self):
-        import ncatbot_config as ncbt
-        self.read_config(ncbt.config)
+        self.read_config(ncatbot_config.attribute_dict)
         super().__init__()
         
-        _log = get_log("Bot")
+        self._ncatbot_logger = get_log("Bot")
     
     @staticmethod
     def read_config(bot_config: dict) -> None:
-        config.set_ws_uri(bot_config["ws_uri"])        # Set napcat websocket server addr
-        config.set_token(bot_config["token"])          # Set websocket token (napcat wsserver token)
-        config.set_webui_uri(bot_config["webui_uri"])  # Set webui addr
-        config.set_bot_uin(bot_config["bot_uin"])      # Set bot qq
-        config.set_root(bot_config["root"])            # Set bot rooter account
+        config.set_ws_uri(bot_config.get("ws_uri"))        # Set napcat websocket server address
+        config.set_token(bot_config.get("token"))          # Set websocket token (napcat ws-server token)
+        config.set_webui_uri(bot_config.get("webui_uri"))  # Set webui address
+        config.set_bot_uin(bot_config.get("bot_uin"))      # Set bot qq
+        config.set_root(bot_config.get("root"))            # Set bot rooter account
