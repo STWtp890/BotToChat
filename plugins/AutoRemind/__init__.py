2486fadc637f7128098dd1310cdc0fa0550213bd
STWtp <1582160745@qq.com>
2025-09-01 05:14:35 +0800

    feature: 将AsyncTask更名为AutoRemind, 增加了可基于yaml配置注册任务的功能, 增加了实时对话场合注册任务的功能, 增加了热重载yaml配置任务的功能, 实时注册任务使用BasePlugin内置的数据持久化

---
+++ plugins/AutoRemind/__init__.py
@@ -0,0 +1,3 @@
+from .main import AutoRemind
+
+# __all__ = ["AutoRemind",]
---
+++ plugins/AutoRemind/auto_remind_path.py
@@ -0,0 +1,7 @@
+from os.path import (
+    join as os_path_join
+)
+
+from ncatbot_path import plugins_dir
+
+autotask_base_dir = os_path_join(plugins_dir, "AutoRemind")
